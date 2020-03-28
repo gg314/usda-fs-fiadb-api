3b419d043a1899f55a90e271a059092b06b4c291
gg314 <gg314@users.noreply.github.com>
2020-03-28 13:13:29 -0600

    make demo file more state-agnostic; more examples

--- docs/get_state_trees.py
+++ docs/get_state_trees.py
@@ -1,52 +1,66 @@
 import csv
 import sys
+
 sys.path.append("..")
 
 from fiadb import FIADB
 
-def toFloat(f):
-  try:
-    return float(f)
-  except:
-    return 0
 
-FIPS = "55"
-STATE_PREFIX = "wi"
+def toFloat(f):
+    try:
+        return float(f)
+    except:
+        return 0
+
+
+FIPS = "30"
+STATE_PREFIX = "mt"
 
 client = FIADB(verbose=True)
 
 acres_per_county = client.fullreport.get(
     reptype="State",
     snum="Area of sampled land and water, in acres",
-    wc=FIPS+"2018",
+    wc=FIPS + "2018",
     pselected="None",
     rselected="County code and name",
     cselected="EVALID",
 )["row"]
 
-acres = { c["content"][0:5] : c["column"][0]["cellValueNumerator"] for c in (acres_per_county[1:]) }
+acres = {
+    c["content"][0:5]: c["column"][0]["cellValueNumerator"]
+    for c in (acres_per_county[1:])
+}
 
 trees_per_county = client.fullreport.get(
     reptype="State",
     snum="Number of live trees (at least 1 inch d.b.h./d.r.c.), in trees, on forest land",
-    wc=FIPS+"2018",
-    pselected="Species", # For more grouping, less descriptive names: "Species group"
+    wc=FIPS + "2018",
+    pselected="Species",  # For more grouping, less descriptive names: "Species group"
     rselected="County code and name",
     cselected="EVALID",
 )["page"]
 
 
-data = {page["content"]: {col["content"][0:5] : toFloat( col["column"][0]["cellValueNumerator"] ) for col in (page["row"][1:]) } for page in trees_per_county[1:]}
+data = {
+    page["content"]: {
+        col["content"][0:5]: toFloat(col["column"][0]["cellValueNumerator"])
+        for col in (page["row"][1:])
+    }
+    for page in trees_per_county
+}
 
 for page in data:
-  title = page
-  output_matrix = [["FIPS", "PER_COUNTY"]]
-  for county in acres:
-    try:
-      output_matrix.append([county, data[page][county] / acres[county]])
-    except KeyError:
-      output_matrix.append([county, 0])
+    title = "All" if page == "Page total" else page
+    output_matrix = [["FIPS", "PER_COUNTY"]]
+    for county in acres:
+        try:
+            output_matrix.append([county, data[page][county] / acres[county]])
+        except KeyError:
+            output_matrix.append([county, 0])
 
-  with open("data/"+STATE_PREFIX+"_"+title+".csv", "w+", newline='') as output_csv:
-    csvWriter = csv.writer(output_csv, delimiter=",")
-    csvWriter.writerows(output_matrix)
+    with open(
+        "data/" + STATE_PREFIX + "_" + title + ".csv", "w+", newline=""
+    ) as output_csv:
+        csvWriter = csv.writer(output_csv, delimiter=",")
+        csvWriter.writerows(output_matrix)
--- docs/plot_state_trees.py
+++ docs/plot_state_trees.py
@@ -4,30 +4,43 @@ import json
 import re
 import pandas as pd
 
-import plotly.express as px
-from subprocess import call
+# import plotly.express as px
+# from subprocess import call
 import plotly
 
+state_prefix = "mt"
 
 
-plotly.io.orca.config.port = 51111
+# I am having trouble getting orca to start, so I run a server with `orca serve --port 51110 --debug` and then execute the Python code
+plotly.io.orca.config.port = 51110
 print(plotly.io.orca.config)
 print(plotly.io.orca.status)
 
 
 # JSON boundaries come from urlopen("https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json")
+# with urlopen("https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json") as response:
 with open("./geojson-counties-fips.json") as response:
     counties = json.load(response)
 
 
 counter = 0
-files = glob.glob("./data/co_*.csv")
+files = glob.glob("data/" + state_prefix + "_*.csv")
 for filename in files:
     counter += 1
-    match = re.search(r"co_(.*)\.csv", filename)
+
+    match = re.search((state_prefix + "_(.*).csv"), filename)
     if match:
+
+        import plotly.express as px
+
         df = pd.read_csv(filename, dtype={"FIPS": str})
 
+        print("Saving %s" % match.group(1))
+
+        # California: {"lat": 37.216, "lon": -119.72}, width="700", height="1050",
+        # Colorado:  {"lat": 39.14, "lon": -105.33}, width="?", height="?",
+        # Montana:  {"lat": 47.217, "lon": -110.087}, width="?", height="?",
+        # Wisconsin:  {"lat": 44.8, "lon": -89.4173}, width="750", height="650",
 
         lbl = "PER_COUNTY"
         fig = px.choropleth_mapbox(
@@ -35,18 +48,35 @@ for filename in files:
             geojson=counties,
             locations="FIPS",
             color=lbl,
-            color_continuous_scale="Blugrn",
+            color_continuous_scale="tempo",  # "mint" or others
             zoom=5.4,
-            center={"lat": 39.7, "lon": -104.99},
+            center={"lat": 47.217, "lon": -110.087},
             opacity=1,
             labels={lbl: match.group(1) + " per acre"},
             mapbox_style="white-bg",  # "carto-positron"
         )
         fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
-        fig.update_layout({"title": {"text": match.group(1), "font": {"size": 38, "family": "Harmonia Sans Pro"}, 'x': 0.46, 'y': 0.85, 'xanchor': 'center', 'yanchor': 'top'}})
+        fig.update_layout(
+            {
+                "title": {
+                    "text": match.group(1),
+                    "font": {"size": 38, "family": "Harmonia Sans Pro"},
+                    "x": 0.46,
+                    "y": 0.93,
+                    "xanchor": "center",
+                    "yanchor": "top",
+                }
+            }
+        )
         fig.update_coloraxes(showscale=False)
         fig.update_layout(showlegend=False)
 
-        #fig.show()
-        fig.write_image(file="./results/co_"+match.group(1)+".png", format="png", width="750", height="575", validate=True)
+        #      fig.show() # show interactive figure in browser
+        fig.write_image(
+            file="./results/" + state_prefix + "_" + match.group(1) + ".png",
+            format="png",
+            width="750",
+            height="650",
+            validate=False,
+        )
         print("Saved %s (%d/%d)" % (match.group(1), counter, len(files)))
